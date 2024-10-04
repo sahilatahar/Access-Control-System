45b1bcbcdb8fb55140dbf21c8933ce610cdb4128
Sahil Atahar <sahilatahar@gmail.com>
2024-10-04 12:39:51 +0530

    fix: build error type unknown

--- server/config/dbConfig.ts
+++ server/config/dbConfig.ts
@@ -5,7 +5,11 @@ const connectDB = async () => {
 		const conn = await mongoose.connect(process.env.MONGO_URI!)
 		console.log(`MongoDB Connected: ${conn.connection.host}`)
 	} catch (error: any) {
-		console.error(`Error: ${error.message}`)
+		if (error instanceof Error) {
+			console.error("Database connection failed:", error.message)
+		} else {
+			console.error("An unexpected error occurred:", error)
+		}
 		process.exit(1)
 	}
 }
